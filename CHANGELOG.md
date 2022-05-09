d830af0df91bfdb5fc580ad19ad4802d15566501
YevheniiRybak <yevhenii.rybak@fourmeta.com>
2022-05-09 09:23:16 +0300

    Fixed generating classes
    Fixed generating classes who has several properties

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,17 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project mostly adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.0.3] - 2022-05-09
+
+### Fixed
+- Fixed generating classes who has several properties. For, example: py, px, mx, my
+
+## [1.0.2] - 2022-05-07 
+- Preparing to upload to npm
+
+## [1.0.1] - 2022-05-07 
+- Preparing to upload to npm
+
 ## [1.0.0] - 2022-05-07
 
-Initial release
+- Initial release
--- index.js
+++ index.js
@@ -2,20 +2,23 @@ const postcss = require("postcss");
 const plugin = require("tailwindcss/plugin");
 
 module.exports = plugin.withOptions(() => {
-  return function ({ addVariant }) {
-    addVariant("rem", ({ container }) => {
-      let prop;
-      let value;
+	return function ({ addVariant }) {
+		addVariant('rem', ({container}) => {
+			let styles = [];
 
-      container.walkRules((rule) => {
-        rule.walkDecls((decl) => {
-          value = decl.value.split("px")[0];
-          prop = decl.prop;
-        });
-
-        rule.removeAll();
-        rule.prepend(postcss.decl({ prop, value: `${value / 16}rem` }));
-      });
-    });
-  };
+			container.walkRules((rule) => {
+				rule.walkDecls((decl) => {
+					styles.push({
+						value: decl.value.split('px')[0],
+						prop: decl.prop
+					});
+				});
+				rule.removeAll();
+				
+				styles.forEach(style => {
+					rule.prepend(postcss.decl({prop: style.prop, value: `${style.value / 16}rem`}));
+				});
+			});
+		});
+	};
 });
